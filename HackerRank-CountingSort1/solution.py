0c6b5abc7d4a8f682d8a439332d13865da20d059
shayaankhatri-dev <87584839+shayaankhatri-dev@users.noreply.github.com>
2024-03-20 04:59:30 +0500

    Solution for Counting Sort 1

---
+++ HackerRank-CountingSort1/solution.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    li = []
+    
+    count = 0
+    for i in range(100):
+        for j in arr:
+            if i == j:
+                count = count+1
+        li.append(count)
+        count = 0
+    return li
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
